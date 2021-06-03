2bf3acdd5cfbd7441d2c315165fbbff1c7fb14c0
19048236JoanTeoSongQI <19048236@myrp.edu.sg>
2021-06-03 21:57:43 +0800

    done

--- app/src/main/java/sg/edu/rp/c346/id19048236/practical2/LocationDetails.java
+++ app/src/main/java/sg/edu/rp/c346/id19048236/practical2/LocationDetails.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 
 public class LocationDetails extends AppCompatActivity {
     TextView textDetails1,textDetails2,textDetails3,textDetails4,textDetails5,textDetails6;
-    String textClick;
+    TextView textUrl1,textUrl2,textUrl3,textUrl4,textUrl5,textUrl6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +34,13 @@ public class LocationDetails extends AppCompatActivity {
         textDetails6=findViewById(R.id.textViewDetails6);
         registerForContextMenu(textDetails6);
 
+        textUrl1=findViewById(R.id.textViewUrl1);
+        textUrl2=findViewById(R.id.textViewUrl2);
+        textUrl3=findViewById(R.id.textViewUrl3);
+        textUrl4=findViewById(R.id.textViewUrl4);
+        textUrl5=findViewById(R.id.textViewUrl5);
+        textUrl6=findViewById(R.id.textViewUrl6);
+
 
 
         Intent i=getIntent();
@@ -42,111 +49,104 @@ public class LocationDetails extends AppCompatActivity {
         String url= i.getStringExtra("value2");
 
 
-        textDetails1.setText(title +getString(R.string.place1)+"\n"+description+getString(R.string.description1)+"\n"+url+getString(R.string.url1));
-        textDetails2.setText(title +getString(R.string.place2)+"\n"+description+getString(R.string.description2)+"\n"+url+getString(R.string.url2));
-        textDetails3.setText(title +getString(R.string.place3)+"\n"+description+getString(R.string.description3)+"\n"+url+getString(R.string.url3));
-        textDetails4.setText(title +getString(R.string.place4)+"\n"+description+getString(R.string.description4)+"\n"+url+getString(R.string.url4));
-        textDetails5.setText(title +getString(R.string.place5)+"\n"+description+getString(R.string.description5)+"\n"+url+getString(R.string.url5));
-        textDetails6.setText(title +getString(R.string.place6)+"\n"+description+getString(R.string.description6)+"\n"+url+getString(R.string.url6));
+        textDetails1.setText(title +getString(R.string.place1)+"\n"+description+"\n"+getString(R.string.description1));
+        textDetails2.setText(title +getString(R.string.place2)+"\n"+description+"\n"+getString(R.string.description2));
+        textDetails3.setText(title +getString(R.string.place3)+"\n"+description+"\n"+getString(R.string.description3));
+        textDetails4.setText(title +getString(R.string.place4)+"\n"+description+"\n"+getString(R.string.description4));
+        textDetails5.setText(title +getString(R.string.place5)+"\n"+description+"\n"+getString(R.string.description5));
+        textDetails6.setText(title +getString(R.string.place6)+"\n"+description+"\n"+getString(R.string.description6));
 
-  /*      Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url1)));
-        Intent intentWeb2 = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url2)));
-        Intent intentWeb3 = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url3)));
-        Intent intentWeb4 = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url4)));
-        Intent intentWeb5 = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url5)));
-        Intent intentWeb6 = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url6)));
+        textUrl1.setText(url+getString(R.string.url1));
+        textUrl2.setText(url+getString(R.string.url2));
+        textUrl3.setText(url+getString(R.string.url3));
+        textUrl4.setText(url+getString(R.string.url4));
+        textUrl5.setText(url+getString(R.string.url5));
+        textUrl6.setText(url+getString(R.string.url6));
 
-        startActivity(intentWeb);
-        startActivity(intentWeb2);
-        startActivity(intentWeb3);
-        startActivity(intentWeb4);
-        startActivity(intentWeb5);
-        startActivity(intentWeb6);
+        textUrl1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url1)));
+                startActivity(intentWeb);
+            }
+        });
 
-
-   */
+        textUrl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url2)));
+                startActivity(intentWeb);
+            }
+        });
+        textUrl3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url3)));
+                startActivity(intentWeb);
+            }
+        });
+        textUrl4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url4)));
+                startActivity(intentWeb);
+            }
+        });
+        textUrl5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url5)));
+                startActivity(intentWeb);
+            }
+        });
+        textUrl6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url6)));
+                startActivity(intentWeb);
+            }
+        });
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
- /*   @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-
-        getMenuInflater().inflate(R.menu.main_menu,menu);
-        if (v == textDetails1) {
-            textClick="description1";
-        }
-        else if(v==textDetails2){
-            textClick="description2";
-
-        }
-        else if(v==textDetails3){
-            textClick="description3";
-        }
-        else if(v==textDetails4){
-            textClick="description4";
-        }
-        else if(v==textDetails5){
-            textClick="description5";
-        }
-        else if(v==textDetails6){
-            textClick="description6";
-        }
+        getMenuInflater().inflate(R.menu.main_menu, menu);
     }
-
-
-  */
-
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id=item.getItemId();
+    public boolean onContextItemSelected(MenuItem item) {
 
-        if(id==R.id.EnglishSelection){
-            Intent i=getIntent();
-            String title= i.getStringExtra("value");
-            String description= i.getStringExtra("value1");
-            String url= i.getStringExtra("value2");
+        int id = item.getItemId();
+        Intent i=getIntent();
+        String title= i.getStringExtra("value");
+        String description= i.getStringExtra("value1");
+        String title2= i.getStringExtra("value3");
+        String description2=i.getStringExtra("value4");
 
-            textDetails1.setText(title +getString(R.string.place1)+"\n"+description+getString(R.string.description1)+"\n"+url+getString(R.string.url1));
-            textDetails2.setText(title +getString(R.string.place2)+"\n"+description+getString(R.string.description2)+"\n"+url+getString(R.string.url2));
-            textDetails3.setText(title +getString(R.string.place3)+"\n"+description+getString(R.string.description3)+"\n"+url+getString(R.string.url3));
-            textDetails4.setText(title +getString(R.string.place4)+"\n"+description+getString(R.string.description4)+"\n"+url+getString(R.string.url4));
-            textDetails5.setText(title +getString(R.string.place5)+"\n"+description+getString(R.string.description5)+"\n"+url+getString(R.string.url5));
-            textDetails6.setText(title +getString(R.string.place6)+"\n"+description+getString(R.string.description6)+"\n"+url+getString(R.string.url6));
+        if(description.equalsIgnoreCase("Description: ")){
+            if(id == R.id.EnglishSelection){
+                textDetails1.setText(title+getString(R.string.place1)+"\n"+description+"\n"+getString(R.string.description1));
+                textDetails2.setText(title+getString(R.string.place2)+"\n"+description+"\n"+getString(R.string.description2));
+                textDetails3.setText(title+getString(R.string.place3)+"\n"+description+"\n"+getString(R.string.description3));
+                textDetails4.setText(title+getString(R.string.place4)+"\n"+description+"\n"+getString(R.string.description4));
+                textDetails5.setText(title+getString(R.string.place5)+"\n"+description+"\n"+getString(R.string.description5));
+                textDetails6.setText(title+getString(R.string.place6)+"\n"+description+"\n"+getString(R.string.description6));
+                return true;
+            }
+            else if(id == R.id.ChineseSelection){
 
-
-
-
-
+                textDetails1.setText(title2+getString(R.string.place1)+"\n"+description2+"\n"+getString(R.string.desChinese1));
+                textDetails2.setText(title2+getString(R.string.place2)+"\n"+description2+"\n"+getString(R.string.desChinese2));
+                textDetails3.setText(title2+getString(R.string.place3)+"\n"+description2+"\n"+getString(R.string.desChinese3));
+                textDetails4.setText(title2+getString(R.string.place4)+"\n"+description2+"\n"+getString(R.string.desChinese4));
+                textDetails5.setText(title2+getString(R.string.place5)+"\n"+description2+"\n"+getString(R.string.desChinese5));
+                textDetails6.setText(title2+getString(R.string.place6)+"\n"+description2+"\n"+getString(R.string.desChinese6));
+                return true;
+            }
         }
-        else if(id== R.id.ChineseSelection){
-            Intent i=getIntent();
-            String title= i.getStringExtra("value3");
-            String description= i.getStringExtra("value4");
-            String url= i.getStringExtra("value5");
-            textDetails1.setText(title +getString(R.string.place1)+"\n"+description+getString(R.string.desChinese1)+"\n"+url+getString(R.string.url1));
-            textDetails2.setText(title +getString(R.string.place2)+"\n"+description+getString(R.string.desChinese2)+"\n"+url+getString(R.string.url2));
-            textDetails3.setText(title +getString(R.string.place3)+"\n"+description+getString(R.string.desChinese3)+"\n"+url+getString(R.string.url3));
-            textDetails4.setText(title +getString(R.string.place4)+"\n"+description+getString(R.string.desChinese4)+"\n"+url+getString(R.string.url4));
-            textDetails5.setText(title +getString(R.string.place5)+"\n"+description+getString(R.string.desChinese5)+"\n"+url+getString(R.string.url5));
-            textDetails6.setText(title +getString(R.string.place6)+"\n"+description+getString(R.string.desChinese6)+"\n"+url+getString(R.string.url6));
-
-
-
-
-        }
-
-        return super.onOptionsItemSelected(item);
+        return super.onContextItemSelected(item);
     }
-
-
 
 }
